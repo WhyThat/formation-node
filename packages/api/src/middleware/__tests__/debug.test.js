6580647527117d874c79ea0aa7f80c30ce9c339b
Mathieu HATON <mathieu.haton@link-value.fr>
2019-03-25 20:00:21 +0100

    test(debugMiddleware): initial test

---
+++ packages/api/src/middleware/__tests__/debug.test.js
@@ -0,0 +1,28 @@
+import debugMiddleware from '../debug'
+
+describe('debugMiddleware', () => {
+  const oldLog = console.log
+  beforeEach(() => {
+    console.log = jest.fn()
+    console.log.mockReset()
+  })
+  it('should call console.log because is in `process.env.DEBUG=true`', () => {
+    process.env.DEBUG = true
+    const next = jest.fn()
+    debugMiddleware({ body: 'body', originalUrl: '/walou' }, '', next)
+    // eslint-disable-next-line no-useless-escape
+    expect(console.log).toHaveBeenCalledWith('/walou : ', JSON.stringify('body'))
+    expect(next).toHaveBeenCalled()
+  })
+  it('should not call console.log because `process.env.DEBUG=false`', () => {
+    const next = jest.fn()
+    process.env.DEBUG = false
+    debugMiddleware({ body: 'body', originalUrl: '/walou' }, '', next)
+    // eslint-disable-next-line no-useless-escape
+    expect(console.log).not.toHaveBeenCalled()
+    expect(next).toHaveBeenCalled()
+  })
+  afterAll(() => {
+    console.log = oldLog
+  })
+})
